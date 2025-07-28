97a4f6d48b203a2b01180576d238c897f877b41e
Krabes Marcel <Marcel.Krabes@stud.bs7-augsburg.de>
2025-07-28 09:36:47 +0200

    shüsh

--- Feiertage.java
+++ Feiertage.java
@@ -1,7 +1,19 @@
 import javax.swing.JOptionPane;
-public static void main (String[] args) {
-    int jahr;
-    String UserInput = JOptionPane.showInputDialog("Geben sie ein Jahr ein");
-    jahr = Integer.parseInt(UserInput);
+public class Feiertage {
+    int jahr = jahreseingabe();
+    public int jahreseingabe(){
+        int jahr;
+        String UserInput = JOptionPane.showInputDialog("Geben sie ein Jahr ein");
+        return jahr = Integer.parseInt(UserInput);
+    }
+    public void Osterrechner(){
+        int K = jahr/100;
+        int M = 15+((3*K+3)/4)-((8*K+13)/25);
+        int c = jahreszahl % 7;
+        int monat = 0;
+    }
+    public static void main (String[] args) {
 
+    }
 }
+    
